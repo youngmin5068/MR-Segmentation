eaef13295f14d1f358cb766d090058f24b155736
youngmin5068 <youngmin5068@naver.com>
2023-11-02 04:16:44 +0000

    add : roi 모델 저장 경로 추가

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 EPOCHS = 100
 BATCH_SIZE = 32
-LEARNINGRATE = 0.0001
+LEARNINGRATE = 0.001
 TRAIN_PATH = "/mount_folder/sampling/train/balance"
 TUNING_PATH = "/mount_folder/sampling/tuning/balance"
 TEST_PATH = "/mount_folder/sampling/test/balance"
-DIR_CHECKPOINT = '/workspace/IITP/MR-Segmenation/CHECKPOINT_tumor'
+DIR_CHECKPOINT = '/workspace/MR-Segmentation/CHECKPOINT_tumor'
+B_DIR_CHECKPOINT = '/workspace/MR-Segmentation/dir_checkpoint_breast_ROI'
 ROI_MODEL_PATH = '/workspace/MR-Segmentation/dir_checkpoint_breast_ROI/ROI_cbam-swinUNetr.pth'
