f0ca2c4d7838bd4ab948a2b0c799b537a40287f7
youngmin5068 <youngmin5068@naver.com>
2023-11-03 01:57:12 +0000

    edit : BATCH_SIZE 변경

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 EPOCHS = 100
-BATCH_SIZE = 32
-LEARNINGRATE = 0.001
-TRAIN_PATH = "/mount_folder/sampling/train/balance"
-TUNING_PATH = "/mount_folder/sampling/tuning/balance"
-TEST_PATH = "/mount_folder/sampling/test/balance"
+BATCH_SIZE = 16
+LEARNINGRATE = 0.0001
+
+TRAIN_PATH = "/mount_folder/Tumors/train/undersampling"
+TUNING_PATH = "/mount_folder/Tumors/tuning/undersampling"
+TEST_PATH = "/mount_folder/Tumors/test/undersampling"
+
 DIR_CHECKPOINT = '/workspace/MR-Segmentation/CHECKPOINT_tumor'
 B_DIR_CHECKPOINT = '/workspace/MR-Segmentation/dir_checkpoint_breast_ROI'
-ROI_MODEL_PATH = '/workspace/MR-Segmentation/dir_checkpoint_breast_ROI/ROI_cbam-swinUNetr.pth'
+ROI_MODEL_PATH = "/workspace/MR-Segmentation/dir_checkpoint_breast_ROI/ROI_Model_231102.pth"
